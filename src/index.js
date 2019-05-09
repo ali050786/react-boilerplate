42b1a96b0e732cf82e84463e767cc51692886d04
Sikandar <ali050786@gmail.com>
2019-05-10 03:18:57 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const mainApp = document.getElementById('app');
+
+ReactDOM.render(<App/>, mainApp);
